beb69c0aeddbc8fa11a230b7f0291db92622d22b
iamkanako17 <kanako.kk8787tw@gmail.com>
2020-09-08 14:13:49 +0900

    nicknameのバリデーション変更

--- app/models/host_user.rb
+++ app/models/host_user.rb
@@ -5,23 +5,23 @@ class HostUser < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   # has_one :inn_description
-
+  
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
-    validates :nickname
     validates :hostname
   end
-
+  
   with_options presence: true, format: { with: /\A[ア-ヶ一ー]+\z/ } do
     validates :hostname_kana
   end
-
+  
   with_options presence: true do
+    validates :nickname
     validates :phone, format: { with: /[0-9]{6,11}/ }, length: { maximum: 11 }
     validates :age
     validates :gender
     validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/ }
   end
 
-  PASSWORD_REGEX= /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, presence: true, format: PASSWORD_REGEX, length: { minimum: 6 }
 end
