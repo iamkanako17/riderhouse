7afc95b7d74a17f8de064637a1a00464997ac0ee
iamkanako17 <kanako.kk8787tw@gmail.com>
2020-09-22 16:30:34 +0900

    controllerの修正

--- app/controllers/lodgings_controller.rb
+++ app/controllers/lodgings_controller.rb
@@ -1,5 +1,5 @@
 class LodgingsController < ApplicationController
-  before_action :set_lodge, only: [:show]
+  before_action :set_lodge, only: [:show, :edit, :destroy]
 
   def index
     @lodges = Lodging.all.order('created_at DESC')
@@ -20,12 +20,36 @@ class LodgingsController < ApplicationController
   end
 
   def show
+    @lat = @lodge.latitude
+    @lng = @lodge.longitude
   end
+
+  def edit
+  end
+
+  def update
+    lodge = Lodging.find(lodge_params)
+    if lodge.valid?
+      lodge.update(lodge_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @lodge.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
 
   private
 
   def lodge_params
-    params.require(:lodging).permit(:lodge_name, :price, :postcode, :prefecture_id, :city, :block_number, :building, images: []).merge(host_user_id: current_host_user.id)
+    params.require(:lodging).permit(:lodge_name, :price, :postcode, :prefecture_city, :latitude, :longitude,:block_number, :building, :description, images: []).merge(host_user_id: current_host_user.id)
   end
 
   def set_lodge
